e96e73d15899a634fadfac93e3d0a023142fe6b9
yushicheng <1542874601@qq.com>
2022-06-28 16:50:31 +0800

    temp:临时节点标记 星期二 下午 4:50:31 六月 28日 2022 创建了 src/components/inputer/ImageUploadInput/index.js 修改了 src/components/inputer/ResourceLinkInput/index.js

--- src/components/inputer/ImageUploadInput/index.js
+++ src/components/inputer/ImageUploadInput/index.js
@@ -10,19 +10,24 @@ import React, { useState, useCallback } from "react";
 // import css from "./style.less";
 
 import upload_request from "@/utils/request/upload_request";
-import get_filelist_detail from "./utils/get_filelist_detail";
+import get_filelist_detail from "@/utils/upload/get_filelist_detail";
 
-export default function UploadInput(props) {
+export default function ImageUploadInput(props) {
 
   const { value, onChange, ...otherProps } = props;
 
   const [file_list, set_file_list] = useState(get_filelist_detail(value));
 
-  const handleChange = useCallback(async (file_object) => {
+  const handleAction = useCallback(async ({ file: file_object }) => {
     const oss_url = await upload_request(file_object);
     await set_file_list(get_filelist_detail(oss_url));
     await onChange(oss_url);
+    return oss_url;
   }, [onChange]);
+
+  const handleChange = useCallback((info) => {
+    console.log(info);
+  }, []);
 
   const handleRemove = useCallback(async () => {
     await set_file_list([]);
@@ -31,10 +36,12 @@ export default function UploadInput(props) {
 
   return (
     <Upload.Dragger
+      accept=".png,.jpg,.jpeg,.gif"
       showUploadList
       fileList={file_list}
       style={{ width: "50%" }}
-      action={handleChange}
+      customRequest={handleAction}
+      onChange={handleChange}
       onRemove={handleRemove}
     >
       <p className="ant-upload-drag-icon">
@@ -44,19 +51,17 @@ export default function UploadInput(props) {
         点击该区域上传文件
       </p>
       <p className="ant-upload-hint">
-        Support for a single or bulk upload. Strictly prohibit from uploading company data or other
-        band files
+        支持png,jpg,jpeg和gif类型的文件
       </p>
     </Upload.Dragger>
   )
 };
 
 
-UploadInput.propTypes = {
-
+ImageUploadInput.propTypes = {
 
 };
-UploadInput.defaultProps = {
+ImageUploadInput.defaultProps = {
   value: "",
   onChange() { }
 };
--- src/components/inputer/ResourceLinkInput/index.js
+++ src/components/inputer/ResourceLinkInput/index.js
@@ -6,28 +6,37 @@ import React from "react";
 
 // import css from "./style.scss";
 // import css from "./style.less";
-import UploadInput from "@/components/inputer/UploadInput";
 import RemoteURLInput from "@/components/inputer/RemoteURLInput";
-
+import ImageUploadInput from "@/components/inputer/ImageUploadInput";
+import VideoUploadInput from "@/components/inputer/VideoUploadInput";
 
 export default function ResourceLinkInput(props) {
   const { content_type, resource_type, value, onChange, ...otherProps } = props;
+
+  if (resource_type === "CUSTOM_URL") {
+    return (
+      <RemoteURLInput value={value} onChange={onChange} {...otherProps} />
+    )
+  }
+
+  if (content_type === "VIDEO") {
+    return (
+      <VideoUploadInput value={value} onChange={onChange} {...otherProps} />
+    )
+  }
+
+  if (content_type === "IMAGE") {
+    return (
+      <ImageUploadInput value={value} onChange={onChange} {...otherProps} />
+    )
+  }
 
   if (content_type === "HTML5") {
     return (
       <RemoteURLInput value={value} onChange={onChange} {...otherProps} />
     )
   }
-  if (resource_type === "OSS_URL") {
-    return (
-      <UploadInput value={value} onChange={onChange} {...otherProps} />
-    )
-  }
-  if (resource_type === "CUSTOM_URL") {
-    return (
-      <RemoteURLInput value={value} onChange={onChange} {...otherProps} />
-    )
-  }
+
   return null;
 };
 
